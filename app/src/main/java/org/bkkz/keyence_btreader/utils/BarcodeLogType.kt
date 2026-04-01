7ca31079fe8ad1690d86b15ed4bf46a72bb5e08a
Phrit Nakgate <boss5500216@gmail.com>
2026-04-01 17:32:28 +0700

    fix(csv): edit comma barcode desc

--- app/src/main/java/org/bkkz/keyence_btreader/utils/BarcodeLogType.kt
+++ app/src/main/java/org/bkkz/keyence_btreader/utils/BarcodeLogType.kt
@@ -1,7 +1,7 @@
 package org.bkkz.keyence_btreader.utils
 
 enum class BarcodeLogType(val logCode: Short, val logDesc: String) {
-    BARCODE_RECEIVED(0, "App received barcode, waiting to send to controller."),
-    BARCODE_SENT_VIA_BLUETOOTH(1, "Barcode sent to controller, waiting for response."),
+    BARCODE_RECEIVED(0, "App received barcode and wait to send to controller."),
+    BARCODE_SENT_VIA_BLUETOOTH(1, "Barcode sent to controller and wait for response."),
     BARCODE_TRANSFER_SUCCESS(2, "Barcode transferred to central platform successfully.")
 }
